4ab8b1a8324740a6124b3534c94d3f447deb5333
DuyPNC1022I1 <duypn1408@gmail.com>
2022-12-08 16:31:37 +0700

    Add bài tập Static

---
+++ src/staticmethodproperty/Test.java
@@ -0,0 +1,11 @@
+package staticmethodproperty;
+public class Test {
+    public static void main(String[] args) {
+        // Khởi tạo đối tượng
+        Student test = new Student();
+
+        // Truy cập đến phương thức set
+        test.setClasses("C10");
+        test.setName("Quân");
+    }
+}
